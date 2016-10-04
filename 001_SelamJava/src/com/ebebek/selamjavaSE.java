b2b4b409d7ab2e025511b1a92409f8bb5bb1e7f6
Mucahit Emre Tekgul <m.tekgul@ebebek.com>
2016-10-04 15:43:16 +0300

    First Comment

---
+++ 001_SelamJava/src/com/ebebek/selamjavaSE.java
@@ -0,0 +1,18 @@
+package com.ebebek;
+
+public class selamjavaSE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		//  -EKRANA MERHABA YAZDIRMA-
+		
+		 System.out.println("Merhaba \n"
+				+ "hi \n"
+				+ "ho\n"
+				+ "hu \n");
+
+		
+	}
+
+}
